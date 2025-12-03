aa9d9811041a3f391c17c3f1dd75862721eaf5b0
Samhith Enganti <enganti.samhith@gmail.com>
2025-12-03 00:29:01 -0500

    Refactor kernel_main for clarity and remove unused code

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,9 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use bootloader_api::{BootInfo, entry_point};
 use core::fmt::Write;
-use x86_64::instructions::{nop, port::Port, hlt};
+use x86_64::instructions::{port::Port, hlt};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
@@ -19,38 +19,30 @@ pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
     }
 
     loop {
-        nop();
+        hlt();
     }
 }
 
 pub fn serial() -> uart_16550::SerialPort {
-    let mut port = unsafe { uart_16550::SerialPort::new(0x3F8) };
-    port.init();
-    port
+    let mut serial_port = unsafe { uart_16550::SerialPort::new(0x3F8) };
+    serial_port.init();
+    serial_port
 }
 
 entry_point!(kernel_main);
 
-static HELLO: &[u8] = b"Hello World!";
-
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     let mut port = serial();
-    writeln!(port, "Entered kernel with boot info: {boot_info:?}").unwrap();
-    writeln!(port, "\nhello, world\n").unwrap();
+    writeln!(port, "Boot info: {boot_info:?}").ok();
+    writeln!(port, "hello, world").ok();
 
-    // NOTE: The VGA text buffer at 0xb8000 is not available!
     if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
         let info = framebuffer.info();
-        let buffer = framebuffer.buffer_mut();
 
-        // 2. Iterate over every pixel on the screen
-        // The buffer is just a long slice of u8 bytes.
-        // We jump by 'bytes_per_pixel' (which is 3) to get to the next pixel.
-        for pixel in buffer.chunks_exact_mut(info.bytes_per_pixel) {
-            // 3. Write the color. Format is BGR (Blue, Green, Red) based on your logs.
-            pixel[0] = 255; // Blue
-            pixel[1] = 0; // Green
-            pixel[2] = 0; // Red
+        for pixel in framebuffer.buffer_mut().chunks_exact_mut(info.bytes_per_pixel) {
+            pixel[0] = 255;
+            pixel[1] = 0;
+            pixel[2] = 0;
         }
     }
 
@@ -59,7 +51,6 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     }
 }
 
-/// This function is called on panic.
 #[panic_handler]
 #[cfg(not(test))]
 fn panic(info: &core::panic::PanicInfo) -> ! {
