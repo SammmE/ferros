4b2395701aaee1cc1e005d85d200019487780396
Samhith E <enganti.samhith@gmail.com>
2025-12-04 12:14:13 -0500

    turned println_serial into serial_println! macro for better performance and avoid remaking the serial port every time

--- kernel/src/serial.rs
+++ kernel/src/serial.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
 use x86_64::instructions::{hlt, port::Port};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -24,8 +27,33 @@ pub fn serial() -> uart_16550::SerialPort {
     serial_port
 }
 
-pub fn println_serial(args: core::fmt::Arguments) {
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = Mutex::new(serial());
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
-    let mut serial_port = serial();
-    let _ = serial_port.write_fmt(args);
+    use x86_64::instructions::interrupts;
+
+    interrupts::without_interrupts(|| {
+        SERIAL1
+            .lock()
+            .write_fmt(args)
+            .expect("Printing to serial failed");
+    });
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(concat!($fmt, "\n"), $($arg)*));
 }
